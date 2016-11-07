8c85dad17b8bd85f523381ca8516a45f2cea0b89
motet <lindemer.s@husky.neu.edu>
2016-11-07 16:15:05 -0500

    new data file created in /data for each run of track.py

--- src/track.py
+++ src/track.py
@@ -4,29 +4,39 @@ from MultiWii import MultiWii
 from Pid import Pid
 from time import sleep
 from sys import argv
+from os import listdir
+from os.path import isfile, join
 import csv
 
 pixy_init()
-board = MultiWii("/dev/ttyUSB0")
-datafile = open('data.csv', 'wb')
+board = MultiWii('/dev/ttyUSB0')
+
+path = '/home/pi/anti-drone-system/data'
+filenames = [x for x in listdir(path) if isfile(join(path, x))]
+if len(filenames) != 0:
+        datanum = max([int(x[4:-4]) for x in filenames]) + 1
+else:
+        datanum = 0
+filename = 'data' + str(datanum) + '.csv'
+datafile = open(join(path, filename), 'wb')
 csvwriter = csv.writer(datafile, delimiter=',')
 
 class Blocks (Structure):
-	_fields_ = [ ("type", c_uint),
-		("signature", c_uint),
-		("x", c_uint),
-		("y", c_uint),
-		("width", c_uint),
-		("height", c_uint),
-		("angle", c_uint) ]
+        _fields_ = [ ("type", c_uint),
+                ("signature", c_uint),
+                ("x", c_uint),
+                ("y", c_uint),
+                ("width", c_uint),
+                ("height", c_uint),
+                ("angle", c_uint) ]
 
 blocks = BlockArray(100)
 frame  = 0
 
-roll_offset = 1500		# center roll value
-thrust_offset = 1420		# center thrust value (~hover)
-pixel_x_offset = 160		# center of screen on x-axis
-pixel_y_offset = 100		# center of screen on y-axis
+roll_offset = 1500              # center roll value
+thrust_offset = 1420            # center thrust value (~hover)
+pixel_x_offset = 160            # center of screen on x-axis
+pixel_y_offset = 100            # center of screen on y-axis
 
 time = 0
 dt = 0.05
@@ -63,7 +73,7 @@ while True:
 			roll = roll_pid.get_output(pixel_x_offset) + roll_offset
 			pitch = 1500
 			thrust = thrust_pid.get_output(pixel_y_offset) + thrust_offset
-			yaw = 1500	
+			yaw = 1500
 
 		data = [time, frame, x, y, size, roll, pitch, thrust, yaw]
 		print data
@@ -76,7 +86,7 @@ while True:
 	except KeyboardInterrupt:
 		datafile.close()
 		while True:
-			try:	
+			try:
 				print 'Landing mode. Press CTRL+C to stop.'
 				board.sendCMD(16, MultiWii.SET_RAW_RC, [1500, 1500, 1400, 1500, 1500, 1500, 1500, 1500])
 				sleep(dt)
