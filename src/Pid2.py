0dca13b507db8e025d930a36a2bcfaa20ba74d3b
motet <lindemer.s@husky.neu.edu>
2016-11-13 14:18:15 -0500

    Added noise-resilient PID controller

---
+++ src/Pid2.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+class Pid():
+    def __init__(self, Kp, Ki, Kd, n=10):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.n = n
+        self.store = np.zeros(n)
+        self.iteration = 0
+        self.limit = None
+        self.r = 0
+
+    def set_limit(self, limit):
+        self.limit = limit
+
+    def set_reference(self, r):
+        self.r = r
+
+    def get_output(self, y):
+        e = self.r - y
+        self.store[self.iteration % self.n] = e
+
+        P = self.Kp * e
+        I = self.get_i()
+        D = self.get_d()
+
+        PID = P + I + D
+        if self.limit and PID < -self.limit: PID = -self.limit
+        if self.limit and PID >  self.limit: PID =  self.limit
+
+        self.iteration += 1
+        return PID
+
+    def get_i(self):
+        return self.Ki * sum(self.store)
+
+    def get_d(self):
+        if self.iteration >= self.n:
+            tmp = self.iteration % self.n
+            if tmp <= self.n / 2:
+                b = sum(self.store[tmp:self.n / 2 + tmp])
+                a = sum(self.store) - b
+            else:
+                a = sum(self.store[tmp - self.n / 2:tmp])
+                b = sum(self.store) - a
+            return self.Kd * (a - b)
+        else:
+            return 0
