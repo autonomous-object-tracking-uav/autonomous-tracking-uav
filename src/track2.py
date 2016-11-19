255eb9a9e3db98097a3dbdd3fb5931271317a35d
Oliver Scott <oliverhbscott@gmail.com>
2016-11-19 02:01:20 +0000

    tuned track2.py to a workable level. Stopped working when Oliver almost died

--- src/track2.py
+++ src/track2.py
@@ -20,36 +20,35 @@ else:
 filename = 'data' + str(datanum) + '.csv'
 datafile = open(join(path, filename), 'wb')
 csvwriter = csv.writer(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-csvwriter.writerow(['time', 'frame', 'x', 'y', 'size_inv', 'roll', 'pitch', 'thrust', 'yaw'])
+csvwriter.writerow(['time', 'x', 'y', 'roll', 'pitch', 'thrust', 'yaw'])
 
 blocks = BlockArray(1)
-frame  = 0
 
-landing_thrust = 1175
+landing_thrust = 1250
 
 roll_offset = 1500              # center roll control value
-pitch_offset = 1500             # center pitch control value
-thrust_offset = 1150            # center thrust control value (~hover)
-yaw_offset = 1500		# center yaw control value
+pitch_offset = 1494             # center pitch control value
+thrust_offset = 1480            # center thrust control value (~hover)
+yaw_offset = 1500		        # center yaw control value
 pixel_x_offset = 160            # center of screen on x-axis
 pixel_y_offset = 100            # center of screen on y-axis
 dt = 0.02                       # 50 Hz refresh rate
 
-R_KP = .1
-R_KI = .025
-R_KD = .03
+R_KP = 1.0
+R_KI = 0.0
+R_KD = 0.0
 roll_pid = Pid(R_KP, R_KI, R_KD)
 roll_pid.set_limit(20)
 roll_pid.set_reference(pixel_x_offset)
 
-T_KP = .25
+T_KP = .50
 T_KI = 0.0
 T_KD = 0.0
 thrust_pid = Pid(T_KP, T_KI, T_KD)
 thrust_pid.set_limit(15)
 thrust_pid.set_reference(pixel_y_offset)
 
-Y_KP = 0.1
+Y_KP = 1.0
 Y_KI = 0.0
 Y_KD = 0.0
 yaw_pid = Pid(Y_KP, Y_KI, Y_KD)
@@ -66,45 +65,40 @@ program_start = time.time()
 
 while True:
     try:
-	frame = frame + 1
         loop_start = time.time()
-	count = pixy_get_blocks(1, blocks)
-	if count > 0:
+        count = pixy_get_blocks(1, blocks)
+        if count > 0:
             x = blocks[0].x
             y = blocks[0].y
-	    roll = -roll_pid.get_output(x) + roll_offset
-	    pitch = pitch_offset
-	    thrust = thrust_pid.get_output(y) + thrust_offset
-	    #NOTE: IF YAWING IN WRONG DIRECTION, CHANGE THIS LINE TO yaw = -yaw_pid.get_output(x) + yaw_offset
-	    yaw = yaw_pid.get_output(x) + yaw_offset
-	else:
+            roll = -roll_pid.get_output(x) + roll_offset
+            pitch = pitch_offset
+            thrust = thrust_pid.get_output(y) + thrust_offset
+            yaw = -yaw_pid.get_output(x) + yaw_offset
+        else:
             x = None
             y = None
-	    size_inv = None
-	    roll = -roll_pid.get_output(pixel_x_offset) + roll_offset
-	    pitch = pitch_offset
-	    thrust = thrust_pid.get_output(pixel_y_offset) + thrust_offset
-	    #NOTE: SAME AS ABOVE. CHANGE THIS LINE IF YAW IS REVERSED.
-	    yaw = yaw_pid.get_output(pixel_x_offset) + yaw_offset
+            roll = -roll_pid.get_output(pixel_x_offset) + roll_offset
+            pitch = pitch_offset
+            thrust = thrust_pid.get_output(pixel_y_offset) + thrust_offset
+            yaw = -yaw_pid.get_output(pixel_x_offset) + yaw_offset
 
-	data = [time.time() - program_start, frame, x, y, size_inv, roll, pitch, thrust, yaw]
-	print 'time=%.2f frame=%4d x=%3d y=%3d size_inv=%.7f roll=%4d pitch=%4d thrust=%4d yaw=%4d' % tuple([0.0 if x is None else x for x in data])
-	csvwriter.writerow(data)
+        data = [time.time() - program_start, x, y, roll, pitch, thrust, yaw]
+        print 'time=%.2f x=%3d y=%3d roll=%4d pitch=%4d thrust=%4d yaw=%4d' % tuple([0.0 if x is None else x for x in data])
+        csvwriter.writerow(data)
         command = [roll, pitch, thrust, yaw, 1500, 1500, 1500, 1500]
-	board.sendCMD(16, MultiWii.SET_RAW_RC, command)
-	time.sleep(dt - (loop_start - time.time()))
+        board.sendCMD(16, MultiWii.SET_RAW_RC, command)
+        time.sleep(dt - (loop_start - time.time()))
 
     except KeyboardInterrupt:
-	datafile.close()
-	print 'Landing mode. Press CTRL+C to stop.'
-	while True:
-	    frame = frame + 1
+        print 'Landing mode. Press CTRL+C to stop.'
+        while True:
             loop_start = time.time()
-	    try:
-		board.sendCMD(16, MultiWii.SET_RAW_RC, [1500, 1500, landing_thrust, 1500, 1500, 1500, 1500, 1500])
-		time.sleep(dt - (loop_start - time.time()))
+            try:
+                board.sendCMD(16, MultiWii.SET_RAW_RC, [1500, 1500, landing_thrust, 1500, 1500, 1500, 1500, 1500])
+                time.sleep(dt - (loop_start - time.time()))
             except KeyboardInterrupt:
+                datafile.close()
                 board.disarm()
-	        pixy_close()
-		break
-	break
+                pixy_close()
+                break
+        break
